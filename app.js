e63c51a85e238c38b8a416e26a98cae26d33850e
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2025-03-12 17:46:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ dotenv.config({ path: "./config.env" });
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(bodyParser.json());
